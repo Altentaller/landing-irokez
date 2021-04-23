3c231c66fa5f8957e73496063eb201f8a5fbf65e
altentaller <n.altentaller@gmail.com>
2021-04-23 12:39:40 +0700

    added animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,34 @@
 const nav = document.querySelector('.nav')
+const logo = document.querySelector('.logo')
 window.addEventListener('scroll', fixNav)
 
 function fixNav(){
     if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('nav-active')
+        nav.classList.add('nav-active');
+        logo.classList.add('logo-black');
     } else {
-        nav.classList.remove('nav-active')
+        nav.classList.remove('nav-active');
+        logo.classList.remove('logo-black');
     }
 }  
+
+
+/* to top  */
+
+$(document).ready(function(){   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+    new WOW().init();
+});
